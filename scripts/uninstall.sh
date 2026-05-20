f1ad5d1f41f4064296de07f0d87eb7e4f73b5281
ragnar <ragnar.black@bk.ru>
2026-05-20 19:15:28 +0300

    chore: drop dead commented-out lines
    - scripts/install.sh: stale 'PostUp' alternative using the pass(1)
      password store. We're not using pass and there's no signal that
      anyone was about to.
    - scripts/uninstall.sh: dead 'apt autoremove software-properties-common'.

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -7,7 +7,6 @@ systemctl stop wg-quick@wg0
 systemctl disable wg-quick@wg0
 
 apt autoremove -y wireguard wireguard-dkms wireguard-tools
-#yes | apt autoremove software-properties-common
 apt update
 
 rm -rf /etc/wireguard
